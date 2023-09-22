1cb2ae03392beb1b07de498c5c76879d7d7809d5
TUYIZERE Samuel <samueltuyizere0@gmail.com>
2023-09-22 12:00:51 +0200

    added a function to get basic info about the phone number

--- main.go
+++ main.go
@@ -2,48 +2,62 @@ package validate_rw_phone_numbers
 
 import "strings"
 
-func ValidateMtn(phoneNumber string) bool {
-	if strings.HasPrefix(phoneNumber, "078") || strings.HasPrefix(phoneNumber, "079") {
-		if len(phoneNumber) == 10 {
-			return true
-		}
-		return false
-	}
-	if strings.HasPrefix(phoneNumber, "+25078") || strings.HasPrefix(phoneNumber, "+25079") {
-		if len(phoneNumber) == 13 {
-			return true
-		}
-		return false
-	}
-	if strings.HasPrefix(phoneNumber, "25078") || strings.HasPrefix(phoneNumber, "25079") {
-		if len(phoneNumber) == 12 {
-			return true
-		}
-		return false
-	}
+type PhoneNbrInfo struct {
+	IsValid    bool
+	Telco      string
+	WalletName string
+}
 
+func GetPhoneNumberInfo(phoneNumber string) PhoneNbrInfo {
+	if ValidateMtn(phoneNumber) {
+		return PhoneNbrInfo{
+			true,
+			"MTN",
+			"MTN Mobile Money",
+		}
+	}
+	if ValidateAirtelTigo(phoneNumber) {
+		return PhoneNbrInfo{
+			true,
+			"Airtel/Tigo",
+			"Airtel Money",
+		}
+	}
+	return PhoneNbrInfo{
+		false,
+		"unkown",
+		"unkown",
+	}
+}
+
+func ValidateMtn(phoneNumber string) bool {
+	if strings.HasPrefix(phoneNumber, "078") ||
+		strings.HasPrefix(phoneNumber, "079") {
+		return len(phoneNumber) == 10
+	}
+	if strings.HasPrefix(phoneNumber, "+25078") ||
+		strings.HasPrefix(phoneNumber, "+25079") {
+		return len(phoneNumber) == 13
+	}
+	if strings.HasPrefix(phoneNumber, "25078") ||
+		strings.HasPrefix(phoneNumber, "25079") {
+		return len(phoneNumber) == 12
+	}
 	return false
 }
 
 func ValidateAirtelTigo(phoneNumber string) bool {
-	if strings.HasPrefix(phoneNumber, "072") || strings.HasPrefix(phoneNumber, "073") {
-		if len(phoneNumber) == 10 {
-			return true
-		}
-		return false
+	if strings.HasPrefix(phoneNumber, "072") ||
+		strings.HasPrefix(phoneNumber, "073") {
+		return len(phoneNumber) == 10
 	}
-	if strings.HasPrefix(phoneNumber, "+25072") || strings.HasPrefix(phoneNumber, "+25073") {
-		if len(phoneNumber) == 13 {
-			return true
-		}
-		return false
+	if strings.HasPrefix(phoneNumber, "+25072") ||
+		strings.HasPrefix(phoneNumber, "+25073") {
+		return len(phoneNumber) == 13
 	}
-	if strings.HasPrefix(phoneNumber, "25072") || strings.HasPrefix(phoneNumber, "25073") {
-		if len(phoneNumber) == 12 {
-			return true
-		}
-		return false
+	if strings.HasPrefix(phoneNumber, "25072") ||
+		strings.HasPrefix(phoneNumber, "25073") {
+		return len(phoneNumber) == 12
 	}
-
 	return false
 }
